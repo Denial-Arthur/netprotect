3d124730588bd624a18da0c18d829b8cb34acd77
Denial <remezovdanil240@gmail.com>
2026-05-11 18:01:06 +0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,1 +1,13 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCmk5NdffI5vp5dOyQM3kDGAkEt7byeBGA",
+    authDomain: "netprotect-final.firebaseapp.com",
+    projectId: "netprotect-final",
+    storageBucket: "netprotect-final.firebasestorage.app",
+    messagingSenderId: "1058990579218",
+    appId: "1:1058990579218:web:007b5d39dd101fbeeb82cb"
+};
 
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
+
+console.log('✅ Firebase подключен. База данных готова к приему жалоб.');
